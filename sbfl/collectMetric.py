f0de52c549df9063967c510c49b23af061316261
Instein98 <instein98@gmail.com>
2022-07-15 16:34:37 -0700

    update script for new coverage; add 34 new sbfl formulae

--- sbfl/collectMetric.py
+++ sbfl/collectMetric.py
@@ -1,39 +1,28 @@
 """
 This script is to collect `totalfailed`, `totalpassed`, `failed(s)`, and `passed(s)` for each project or code element
+This script is using the coverage collected during execution of `defects4j test` by TestCovAgent
 """
 
 import os
-import traceback
 import subprocess as sp
 from pathlib import Path
 
 d4jMvnProjDir = '/home/yicheng/apr/d4jMvnForUniapr/d4jMvnProj/'
-d4jProjCoverageDir = '/home/yicheng/apr/flapr/Coverage/Defects4j/'
-allTestsFilesDir = '/home/yicheng/apr/d4jMvnForUniapr/all_tests_files/'
+d4jProjCoverageDir = '/home/yicheng/apr/flapr/Coverage/d4jCov'
+outputDir = 'sbflExeInfo'
 
 # format: fullyQualifiedClassName::testName
 def getSetOfExpectedFailingTest(pid, bid):
-    process = sp.Popen('defects4j info -p {} -b {}'.format(pid, bid),
-                       shell=True, stderr=sp.PIPE, stdout=sp.PIPE, universal_newlines=True)
-    stdout, _ = process.communicate()
-    lines = stdout.strip().split('\n')
-    start = False
     res = set()
-    for line in lines:
-        if 'Root cause in triggering tests:' in line:
-            start = True
-        elif '------------------------------------------------------' in line and start == True:
-            start = False
-            break
-        elif line.startswith(' - ') and start == True:
-            tmp = line[3:]
-            res.add(tmp)
+    expectedFTFile = os.path.join(d4jProjCoverageDir, pid, bid, 'expected_failing_tests')
+    with open(expectedFTFile, 'r') as file:
+        for line in file:
+            res.add(line.strip())
     return res
 
-def getSetOfPassingFailingTests(pid, bid):
+def getSetOfPassingFailingTests(pid, bid, allTestsFilePath):
     allTestsSet = set()
-    allTestsFilePath = os.path.join(allTestsFilesDir, pid, str(bid), 'all_tests')
-    with open(allTestsFilePath) as file:
+    with open(allTestsFilePath, 'r') as file:
         for line in file:
             line = line.strip()
             if line:
@@ -61,45 +50,71 @@ def isTestClass(testSourcePath, dotClassName):
     else:
         return False
 
-def processCoverageFile(pid: str, bid: int, covFilePath: str, passingTestSet: set, failingTestSet: set):
+def readCovFile(filePath):
+    idxDict = {}
+    testDict = {}  # testClass -> {testMethod -> list of idx}
+    with open(filePath, 'r') as file:
+        for line in file:
+            if '->' in line:
+                tmp = line.strip().split(' -> ')
+                idx = tmp[0]
+                element = tmp[1]
+                firstColonIdx = element.index(':')
+                lastColonIdx = element.rindex(':')
+                dotClassName = element[:firstColonIdx].replace("/", ".")
+                lineNum = element[lastColonIdx+1:]
+                newEleStr = dotClassName + ":" + lineNum
+                idxDict[idx] = newEleStr
+            elif ',' in line:
+                tmp = line.strip().split(', ')
+                testName = tmp[0]
+                testDotClassName = testName[:testName.index('#')].replace("/", ".")
+                testMethodName = testName[testName.index('#')+1:]
+                idxList = []
+                for idx in tmp[1:]:
+                    idxList.append(idx)
+                if testDotClassName not in testDict:
+                    testDict[testDotClassName] = {}
+                    testDict[testDotClassName][testMethodName] = idxList
+                else:
+                    testDict[testDotClassName][testMethodName] = idxList
+    return idxDict, testDict
+
+
+def processCoverageFile(pid: str, bid: str, covFilePath: str, passingTestSet: set, failingTestSet: set):
     # format: { codeElement: [set of passing tests covering it, set of failing tests covering it] }
     resDict = {}
     projTestSourceDir = getTestSourceDirPath(pid, bid)
-    with open(covFilePath, 'r') as file:
-        for line in file:
-            try:
-                tmp = line.strip().split()
-                curTest = tmp[0]
-                curTest = curTest[:curTest.index('(')]
-                curTest = curTest[:curTest.rindex('.')] + '::' + curTest[curTest.rindex('.')+1:]
-                passing = True
-                # if this test can't be found 
-                if curTest not in passingTestSet and curTest not in failingTestSet:
-                    continue
-                elif curTest in failingTestSet:
-                    passing = False
-                codeElements = tmp[1:]
-                for element in codeElements:
-                    eleClassName = element[:element.index(':')]
-                    # code elements in test classes should not be considered
-                    if isTestClass(projTestSourceDir, eleClassName):
-                        continue
-                    if element not in resDict.keys():
-                        resDict[element] = [set(), set()]
-                    if passing == True:
-                        resDict[element][0].add(curTest)
-                    else:
-                        resDict[element][1].add(curTest)
-            except:
-                print('[ERROR] Have trouble handling the following line in coverage file {}:\n{}'.format(covFilePath, line))
-                traceback.print_exc()
+    idxDict, testDict = readCovFile(covFilePath)
+
+    for testClass in testDict:
+        for testMethod in testDict[testClass]:
+            curTest = testClass + '::' + testMethod
+            passing = True
+            # if this test can't be found 
+            if curTest not in passingTestSet and curTest not in failingTestSet:
                 continue
+            elif curTest in failingTestSet:
+                passing = False
+            for idx in testDict[testClass][testMethod]:
+                element = idxDict[idx]
+                eleClassName = element[:element.index(':')]
+                # code elements in test classes should not be considered
+                if isTestClass(projTestSourceDir, eleClassName):
+                    continue
+                if element not in resDict.keys():
+                    resDict[element] = [set(), set()]
+                if passing == True:
+                    resDict[element][0].add(curTest)
+                else:
+                    resDict[element][1].add(curTest)
     return resDict
+
 
 def outputCodeElements(outputFilePath: str, elementDict: dict, totalPassedNum: int, totalFailedNum: int, verbose=False):
     """ if verbose, tests covering the code elements will be printed, otherwise only number of such tests will be shown """
     if os.path.isfile(outputFilePath):
-        print("{} already exists, skipping...".format(outputFilePath))
+        print("skipping {}".format(outputFilePath))
         return
     res = 'CodeElement, passed(s), failed(s), totalpassed, totalfailed\n'
     for element in elementDict.keys():
@@ -115,28 +130,27 @@ def outputCodeElements(outputFilePath: str, elementDict: dict, totalPassedNum: i
         file.write(res)
 
 def main():
-    outputDir = 'sbflExeInfo'
     for pid in os.listdir(d4jProjCoverageDir):
         pidPath = os.path.join(d4jProjCoverageDir, pid)
         if not os.path.isdir(pidPath):
             continue
-        for covLog in os.listdir(pidPath):
-            if not covLog.endswith('.txt'):
+        for bid in os.listdir(pidPath):
+            bidPath = os.path.join(pidPath, bid)
+            if not os.path.isdir(bidPath):
                 continue
-            covLogPath = os.path.join(pidPath, covLog)
-            bid = int(covLog[:-4])
+            covLogPath = os.path.join(bidPath, 'coverage.txt')
             outputPath = os.path.join(outputDir, pid, '{}.csv'.format(bid))
             if os.path.isfile(outputPath):
-                print("{} already exists, skipping...".format(outputPath))
+                print("skipping {}".format(outputPath))
                 continue
-            allTestsFilePath = os.path.join(allTestsFilesDir, pid, str(bid), 'all_tests')
+            allTestsFilePath = os.path.join(bidPath, 'all_tests')
             if not os.path.isfile(allTestsFilePath):
                 print('[WARNING] all_tests file not found: {}. Is {}-{} deprecated?'.format(allTestsFilePath, pid, bid))
                 continue
 
             # Start analysis
             print("\n=============== processing {}-{} ===============".format(pid, bid))
-            passingTestSet, failingTestSet = getSetOfPassingFailingTests(pid, bid)
+            passingTestSet, failingTestSet = getSetOfPassingFailingTests(pid, bid, allTestsFilePath)
             resDict = processCoverageFile(pid, bid, covLogPath, passingTestSet, failingTestSet)
             outputCodeElements(outputPath, resDict, len(passingTestSet), len(failingTestSet), verbose=False)
             
--- sbfl/genSuspList.py
+++ sbfl/genSuspList.py
@@ -12,22 +12,158 @@ sbflResultDir = 'sbflResult'
 
 # Formulae
 
-def tarantula(passS: int, failS: int, totalPass: int, totalFail: int):
+def Tarantula(passS: int, failS: int, totalPass: int, totalFail: int):
     numerator = failS / totalFail
     denominator = failS / totalFail + passS / totalPass
     return numerator / denominator
 
-def ochiai(passS: int, failS: int, totalPass: int, totalFail: int):
+def Ample(passS: int, failS: int, totalPass: int, totalFail: int):
+    tmp1 = failS / totalFail
+    tmp2 = passS / totalPass
+    return abs(tmp1 - tmp2)
+
+def rensenDice(passS: int, failS: int, totalPass: int, totalFail: int):
+    numerator = 2 * failS
+    denominator = 2 * failS + passS + (totalFail - failS)
+    return numerator / denominator
+
+def Kulczynski2(passS: int, failS: int, totalPass: int, totalFail: int):
+    tmp1 = failS / totalFail
+    tmp2 = failS / (failS + passS)
+    return (tmp1 + tmp2) / 2
+
+def M1(passS: int, failS: int, totalPass: int, totalFail: int):
+    numerator = failS + (totalPass - passS)
+    demoninator = (totalFail - failS) + passS
+    return numerator / demoninator
+
+def Goodman(passS: int, failS: int, totalPass: int, totalFail: int):
+    numerator = 2*failS - (totalFail - failS) - passS
+    denominator = 2*failS + (totalFail - failS) + passS
+    return numerator / denominator
+
+def Overlap(passS: int, failS: int, totalPass: int, totalFail: int):
+    numerator = failS
+    denominator = min(failS, passS, (totalFail - failS))
+    return numerator / denominator
+
+def Zoltar(passS: int, failS: int, totalPass: int, totalFail: int):
+    numerator = failS
+    denominator = totalFail + passS + (10000*(totalFail-failS)*passS) / failS
+    return numerator / denominator
+
+def ER5c(passS: int, failS: int, totalPass: int, totalFail: int):
+    if failS < totalFail:
+        return 0
+    elif failS == totalFail:
+        return 1
+
+def GP13(passS: int, failS: int, totalPass: int, totalFail: int):
+    return failS * (1 + 1 / (2*passS + failS))
+
+def Dstar2(passS: int, failS: int, totalPass: int, totalFail: int):
+    numerator = failS ** 2
+    denominator = passS + (totalFail - failS)
+    return numerator / denominator
+
+def ER1a(passS: int, failS: int, totalPass: int, totalFail: int):
+    if failS < totalFail:
+        return -1
+    elif failS == totalFail:
+        return totalPass - passS
+
+def Ochiai(passS: int, failS: int, totalPass: int, totalFail: int):
     denominator = math.sqrt(totalFail * (failS + passS))
     return failS / denominator
 
-def op2(passS: int, failS: int, totalPass: int, totalFail: int):
-    tmp = passS / (totalPass + 1)
-    return failS - tmp
+def RussellRao(passS: int, failS: int, totalPass: int, totalFail: int):
+    return failS / (totalFail + totalPass)
 
-def barinel(passS: int, failS: int, totalPass: int, totalFail: int):
-    tmp = passS / (passS + failS)
-    return 1 - tmp
+def Dice(passS: int, failS: int, totalPass: int, totalFail: int):
+    return 2 * failS / (totalFail + passS)
+
+def SimpleMatching(passS: int, failS: int, totalPass: int, totalFail: int):
+    numerator = failS + (totalPass - passS)
+    return numerator / (totalFail + totalPass)
+
+def M2(passS: int, failS: int, totalPass: int, totalFail: int):
+    denominator = failS + (totalPass - passS) + 2*(totalFail - failS) + 2*passS
+    return failS / denominator
+
+def Hamming(passS: int, failS: int, totalPass: int, totalFail: int):
+    return failS + (totalPass - passS)
+
+def Anderberg(passS: int, failS: int, totalPass: int, totalFail: int):
+    denominator = failS + 2*(totalFail - failS) + 2*passS
+    return failS / denominator
+
+def Wong1(passS: int, failS: int, totalPass: int, totalFail: int):
+    return failS
+
+def GP02(passS: int, failS: int, totalPass: int, totalFail: int):
+    return 2*(failS + math.sqrt(totalPass)) + math.sqrt(passS)
+
+def GP19(passS: int, failS: int, totalPass: int, totalFail: int):
+    return failS * math.sqrt(abs( passS - failS + totalFail - totalPass ))
+
+def Wong3(passS: int, failS: int, totalPass: int, totalFail: int):
+    if passS <= 2:
+        h = passS
+    elif 2 < passS <= 10:
+        h = 2 + 0.1 * (passS - 2)
+    elif passS > 10:
+        h = 2.8 + 0.01 * (passS - 10)
+    return failS - h
+
+def ER1b(passS: int, failS: int, totalPass: int, totalFail: int):
+    return failS - passS / (totalPass + 1)
+
+def Jaccard(passS: int, failS: int, totalPass: int, totalFail: int):
+    return failS / (totalFail + passS)
+
+def Hamann(passS: int, failS: int, totalPass: int, totalFail: int):
+    numerator = failS + (totalPass - passS) - passS - (totalFail - failS)
+    return numerator / (totalFail + totalPass)
+
+def Kulczynski1(passS: int, failS: int, totalPass: int, totalFail: int):
+    return failS / ((totalFail - failS) + passS)
+
+def Sokal(passS: int, failS: int, totalPass: int, totalFail: int):
+    numerator = 2 * (failS) + 2 * (totalPass - passS)
+    denominator = 2*failS + 2*(totalPass - passS) + (totalFail - failS) + passS
+    return numerator / denominator
+
+def RogersTanimoto(passS: int, failS: int, totalPass: int, totalFail: int):
+    numerator = failS + (totalPass - passS)
+    denominator = failS + (totalPass - passS) + 2*(totalFail - failS) + 2*passS
+    return numerator / denominator
+
+def Euclid(passS: int, failS: int, totalPass: int, totalFail: int):
+    return math.sqrt(failS + (totalPass - passS))
+
+def Ochiai2(passS: int, failS: int, totalPass: int, totalFail: int):
+    pass_ = totalPass - passS
+    fail_ = totalFail - failS
+    numerator = failS * pass_
+    denominator = math.sqrt((failS + passS) * (fail_ + pass_) * (failS + pass_) * (fail_ + passS))
+    return numerator / denominator
+
+def Wong2(passS: int, failS: int, totalPass: int, totalFail: int):
+    return failS - passS
+
+def GP03(passS: int, failS: int, totalPass: int, totalFail: int):
+    return math.sqrt(abs(failS**2 - math.sqrt(passS)))
+
+def SBI(passS: int, failS: int, totalPass: int, totalFail: int):
+    return failS / (failS + passS)
+
+# def op2(passS: int, failS: int, totalPass: int, totalFail: int):
+#     tmp = passS / (totalPass + 1)
+#     return failS - tmp
+
+# def barinel(passS: int, failS: int, totalPass: int, totalFail: int):
+#     tmp = passS / (passS + failS)
+#     return 1 - tmp
 
 def produceSusList(sbfl, exeCsvFile: str, outputFilePath: str):
     if os.path.isfile(outputFilePath):
@@ -59,7 +195,11 @@ def produceSusList(sbfl, exeCsvFile: str, outputFilePath: str):
             file.write('{}, {}\n'.format(elementScore[0], elementScore[1]))
 
 def main():
-    fls = [tarantula, ochiai, op2, barinel]
+    fls = [Ample, Anderberg, Dice, Dstar2, ER1a, ER1b, ER5c, Euclid, Goodman, 
+            GP02, GP03, GP13, GP19, Hamann, Hamming, Jaccard, Kulczynski1, 
+            Kulczynski2, M1, M2, Ochiai, Ochiai2, Overlap, rensenDice, 
+            RogersTanimoto, RussellRao, SBI, SimpleMatching, Sokal, Tarantula, 
+            Wong1, Wong2, Wong3, Zoltar]
     for pid in os.listdir(exeInfoDir):
         pidPath = os.path.join(exeInfoDir, pid)
         if not os.path.isdir(pidPath):
